9bf22f9530aff3790330d74477c73232730e1f89
Embarkupon <165702416+Embarkupon@users.noreply.github.com>
2024-10-20 13:26:14 -0400

    added button to replace previous grid with a new grid of up to 100 squares per side

--- grid.js
+++ grid.js
@@ -1,18 +1,32 @@
-generateGrid();
+let temp = 16;
+generateGrid(temp);
 
-function generateGrid() {
+const grid_button = document.querySelector(".grid-button");
+grid_button.addEventListener("click", () => {
+    let dimension = prompt("How many squares per side for the grid? (Max 100)");
+    while (typeof dimension != "number" && (dimension > 100 || dimension < 1)) {
+        console.log(typeof dimension + ", " + dimension);
+        dimension = prompt("Please enter a number from '1' to '100'.")
+    }
+    if (dimension != null) {
+        removeGrid(temp);
+        generateGrid(dimension);
+        temp = dimension;
+    }
+});
+
+function generateGrid(grid_size) {
     const grid_container = document.querySelector(".grid-container");
-    const dimension = 16
     const grid_array = [];
     const cell_array = [];
-    const cell_size = 36;
-    const row_width = cell_size*dimension;
+    const cell_size = 32;
+    const row_width = cell_size*grid_size;
     console.log(grid_container);
-    for(let i = 0; i < dimension; i++) {
+    for(let i = 0; i < grid_size; i++) {
         grid_array[i] = document.createElement("div");
         grid_array[i].setAttribute("style", `display: flex; flex-direction: row; width: ${row_width}px; height: ${cell_size}px;`);
         grid_array[i].classList.add(`grid-box`);
-        for(let j = 0; j < dimension; j++) {
+        for(let j = 0; j < grid_size; j++) {
             cell_array[j] = document.createElement("div");
             cell_array[j].setAttribute("style", `border: solid 1px; border-color: black; width: ${cell_size}px; height: ${cell_size}`);
             cell_array[j].classList.add(`grid-cell`);
@@ -31,4 +45,14 @@ function draw(cell_size) {
             cell.setAttribute("style", `background-color: black; border: solid 1px; border-color: black; width: ${cell_size}px; height: ${cell_size}`)
         });
     });
+}
+
+function removeGrid(num) {
+    //num is the previous squares per side of the grid
+    for(let x = 0; x < num; x++) {
+        document.querySelector(".grid-container").removeChild(document.querySelector(".grid-box"));
+    }
+    /*document.querySelectorAll("grid-box").forEach((bruh) => {
+        document.querySelector(".grid-container").removeChild(bruh);
+    });*/
 }
